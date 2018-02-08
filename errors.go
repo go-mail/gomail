816578563d3dc230c3a9bd68ee22052bae52f971
Brian Jones <beeceej.code@gmail.com>
2018-02-07 18:52:55 -0600

    add error doc

--- errors.go
+++ errors.go
@@ -2,13 +2,15 @@ package mail
 
 import "fmt"
 
-// SendError allows access to the underlying error given during a faulty send
+// SendError represents the failure to transmit a Message contains the cause of failure
+// determined by the internal error.
 type SendError struct {
-	Index uint
+	Index uint //Index represents the index of the message which could not be transmitted
 	Cause error
 }
 
 func (err *SendError) Error() string {
+
 	return fmt.Sprintf("gomail: could not send email %d: %v",
 		err.Index, err.Cause)
 }
