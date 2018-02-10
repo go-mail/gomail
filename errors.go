f46a3e875bd0f41b4816d5e2bc310ed07348131b
Brian Jones <beeceej.code@gmail.com>
2018-02-10 11:23:03 -0600

    clean up formatting

--- errors.go
+++ errors.go
@@ -5,12 +5,12 @@ import "fmt"
 // SendError represents the failure to transmit a Message contains the cause of failure
 // determined by the internal error.
 type SendError struct {
-	Index uint //Index represents the index of the message which could not be transmitted
+	//Index represents the index of the message which could not be transmitted
+	Index uint
 	Cause error
 }
 
 func (err *SendError) Error() string {
-
 	return fmt.Sprintf("gomail: could not send email %d: %v",
 		err.Index, err.Cause)
 }
